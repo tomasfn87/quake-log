805e8a9379a8e7d5fffb8eb198f65347749ea29f
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-23 13:40:50 -0300

    Further progression on data parsing.

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,41 @@
 package main
 
-import (
-	"fmt"
-
-	quakeLog "code.repo/quakeLog/src"
-)
-
-func fillWithTimes(char string, size int) string {
-	fill := ""
-	for i := 0; i < size; i++ {
-		fill += fmt.Sprintf("%c", char[0])
-	}
-	return fill
-}
+import quakeLog "code.repo/quakeLog/src"
 
 func main() {
+
 	quakeLogFilePath := "./Quake.txt"
 	file := quakeLog.QuakeLogFile{Path: quakeLogFilePath}
 
 	file.OpenQuakeLog()
+
+	/*
+		x := "a"
+		y := "b"
+		z := "c"
+		list := []string{x, y, z}
+
+				fmt.Println(arrayContains(list, "a"))
+				fmt.Println(arrayContains(list, "b"))
+				fmt.Println(arrayContains(list, "c"))
+				fmt.Println(arrayContains(list, "d"))
+
+			list = updateOldNames(list, "d")
+			fmt.Println(list)
+	*/
+}
+
+/*
+func arrayContains(arr []string, item string) bool {
+	for _, v := range arr {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
+*/
+
+func updateOldNames(oldNames []string, currentName string) []string {
+	return append(oldNames, currentName)
 }
--- src/quake-log.go
+++ src/quake-log.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -26,6 +27,10 @@ type Status struct {
 }
 
 type Players struct {
+	List []Player
+}
+
+type Player struct {
 	Id       int
 	Nome     string
 	Kills    int
@@ -45,24 +50,24 @@ func (qlf QuakeLogFile) OpenQuakeLog() {
 
 	logLines := getFileValues(file)
 
-	quakeGames := []QuakeGameLog{}
+	quakeGames := []*QuakeGameLog{}
 
-	var ClientUserinfoChangedRE *regexp.Regexp
-	ClientUserinfoChangedRE, err = regexp.Compile(`(.*)( n\\)(.+)(\\t\\)(.*)`)
+	var clientUserinfoChangedRE *regexp.Regexp
+	clientUserinfoChangedRE, err = regexp.Compile(`^(.*) (\d+)( n\\)(.+)(\\t\\)(.*)$`)
+	panicIf(err)
 
-	if err != nil {
-		panic(err)
-	}
+	var killRE *regexp.Regexp
+	killRE, err = regexp.Compile(`^(.*)( Kill: )(\d+) (\d+) (\d+:) (.+) killed (.+) by (.*)$`)
+	panicIf(err)
 
 	gameCount := 0
-	totalKillCount := 0
 	for _, v := range logLines {
-		values := strings.Split(v, " ")
-		for _, y := range values {
+		lineContent := strings.Split(v, " ")
+		for _, y := range lineContent {
 			switch y {
 			case "InitGame:":
 				{
-					quakeGames = append(quakeGames, QuakeGameLog{Game: gameCount + 1})
+					quakeGames = append(quakeGames, &QuakeGameLog{Game: gameCount + 1})
 				}
 			case "ShutdownGame:":
 				{
@@ -70,23 +75,64 @@ func (qlf QuakeLogFile) OpenQuakeLog() {
 				}
 			case "ClientUserinfoChanged:":
 				{
-					fmt.Println(ClientUserinfoChangedRE.ReplaceAllString(v, `$3`))
+					clientUserinfoChanged := strings.Split(clientUserinfoChangedRE.ReplaceAllString(v, `$2  :  $4`), "  :  ")
+					id, err := strconv.Atoi(clientUserinfoChanged[0])
+					panicIf(err)
+					id--
+					nome := clientUserinfoChanged[1]
+
+					if playerListContainsId(quakeGames[gameCount].Status.Players.List, id) {
+						pl := getPlayerIndexByIdFromPlayerList(quakeGames[gameCount].Status.Players.List, id)
+
+						if !playerListContainsNome(quakeGames[gameCount].Status.Players.List, nome) {
+							if !oldNamesContainsNome(quakeGames[gameCount].Status.Players.List[pl].OldNames, quakeGames[gameCount].Status.Players.List[pl].Nome) {
+								quakeGames[gameCount].Status.Players.List[pl].OldNames =
+									append(quakeGames[gameCount].Status.Players.List[pl].OldNames, quakeGames[gameCount].Status.Players.List[pl].Nome)
+							}
+							quakeGames[gameCount].Status.Players.List[pl].Nome = nome
+						}
+
+						// fmt.Println(quakeGames[gameCount].Status.Players.List[pl].OldNames)
+
+						if !playerListContainsNome(quakeGames[gameCount].Status.Players.List, nome) {
+							quakeGames[gameCount].Status.Players.List[pl].Nome = nome
+						}
+					} else {
+						quakeGames[gameCount].Status.Players.List = append(quakeGames[gameCount].Status.Players.List, Player{Nome: nome, Id: id})
+					}
+
+					// fmt.Printf("id{%d} type:%T, nome{%s} type:%T\n", id, id, nome, nome)
+
 				}
 			case "Kill:":
 				{
 					quakeGames[gameCount].Status.TotalKills++
-					totalKillCount++
+					kill := strings.Split(killRE.ReplaceAllString(v, `$6  :  $7  :  $8`), "  :  ")
+					killer, victim := kill[0], kill[1]
+					//cause := kill[2]
+					// fmt.Printf("v{%s} k{%s} c{%s}\n", victim, killer, cause)
+					for k, p := range quakeGames[gameCount].Status.Players.List {
+						if killer != "<world>" && killer == p.Nome {
+							quakeGames[gameCount].Status.Players.List[k].Kills++
+						}
+					}
+					if killer == "<world>" {
+						for k, p := range quakeGames[gameCount].Status.Players.List {
+							if p.Nome == victim {
+								quakeGames[gameCount].Status.Players.List[k].Kills--
+							}
+
+						}
+					}
 				}
 			}
 		}
 	}
 
-	/*
-		for _, v := range quakeGames {
-			fmt.Println(v)
-		}
-		fmt.Printf("totalKillCount: %d\n", totalKillCount)
-	*/
+	for _, v := range quakeGames {
+		fmt.Println(v)
+	}
+
 }
 
 func getFileValues(file *os.File) []string {
@@ -99,4 +145,56 @@ func getFileValues(file *os.File) []string {
 		log.Fatal(err)
 	}
 	return logLines
+}
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func playerListContainsId(arr []Player, item int) bool {
+	if len(arr) == 0 {
+		return false
+	}
+	for _, v := range arr {
+		if v.Id == item {
+			return true
+		}
+	}
+	return false
+}
+
+func playerListContainsNome(arr []Player, item string) bool {
+	if len(arr) == 0 {
+		return false
+	}
+	for _, v := range arr {
+		if v.Nome == item {
+			return true
+		}
+	}
+	return false
+}
+
+func getPlayerIndexByIdFromPlayerList(arr []Player, id int) int {
+	var index int
+	for k, v := range arr {
+		if v.Id == id {
+			index = k
+		}
+	}
+	return index
+}
+
+func oldNamesContainsNome(arr []string, nome string) bool {
+	if len(arr) == 0 {
+		return false
+	}
+	for _, v := range arr {
+		if v == nome {
+			return true
+		}
+	}
+	return false
 }
