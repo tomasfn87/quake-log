1d81bb8118613cd49cff6670ed58bc8f5bbda06e
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-23 14:49:27 -0300

    1): Removed file: 'regex.go'. 2): reorganized files 'src/quake-log.go' and 'cmd/main.go'.

--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,17 @@
 package main
 
-import quakeLog "code.repo/quakeLog/src"
+import (
+	"fmt"
+
+	quakeLog "code.repo/quakeLog/src"
+)
 
 func main() {
-
 	quakeLogFilePath := "./Quake.txt"
 	file := quakeLog.QuakeLogFile{Path: quakeLogFilePath}
 
-	file.OpenQuakeLog()
-
-	/*
-		x := "a"
-		y := "b"
-		z := "c"
-		list := []string{x, y, z}
-
-				fmt.Println(arrayContains(list, "a"))
-				fmt.Println(arrayContains(list, "b"))
-				fmt.Println(arrayContains(list, "c"))
-				fmt.Println(arrayContains(list, "d"))
-
-			list = updateOldNames(list, "d")
-			fmt.Println(list)
-	*/
-}
-
-/*
-func arrayContains(arr []string, item string) bool {
-	for _, v := range arr {
-		if v == item {
-			return true
-		}
+	quakeGamesLog := file.OpenQuakeLog()
+	for _, v := range quakeGamesLog {
+		fmt.Println(v)
 	}
-	return false
-}
-*/
-
-func updateOldNames(oldNames []string, currentName string) []string {
-	return append(oldNames, currentName)
 }
--- src/quake-log.go
+++ src/quake-log.go
@@ -2,7 +2,6 @@ package quakeLog
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"regexp"
 	"strconv"
@@ -40,97 +39,18 @@ func NewQuakeLog() *QuakeGameLog {
 	return &QuakeGameLog{}
 }
 
-func (qlf QuakeLogFile) OpenQuakeLog() {
-	file, err := os.Open(qlf.Path)
+func (ql QuakeLogFile) OpenQuakeLog() []QuakeGameLog {
+	file, err := os.Open(ql.Path)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
 	logLines := getFileValues(file)
+	quakeGames := []QuakeGameLog{}
+	quakeGames = parseDataFromFileLines(logLines, quakeGames)
 
-	quakeGames := []*QuakeGameLog{}
-
-	var clientUserinfoChangedRE *regexp.Regexp
-	clientUserinfoChangedRE, err = regexp.Compile(`^(.*) (\d+)( n\\)(.+)(\\t\\)(.*)$`)
-	panicIf(err)
-
-	var killRE *regexp.Regexp
-	killRE, err = regexp.Compile(`^(.*)( Kill: )(\d+) (\d+) (\d+:) (.+) killed (.+) by (.*)$`)
-	panicIf(err)
-
-	gameCount := 0
-	for _, v := range logLines {
-		lineContent := strings.Split(v, " ")
-		for _, y := range lineContent {
-			switch y {
-			case "InitGame:":
-				{
-					quakeGames = append(quakeGames, &QuakeGameLog{Game: gameCount + 1})
-				}
-			case "ShutdownGame:":
-				{
-					gameCount++
-				}
-			case "ClientUserinfoChanged:":
-				{
-					clientUserinfoChanged := strings.Split(clientUserinfoChangedRE.ReplaceAllString(v, `$2   :   $4`), "   :   ")
-					id, err := strconv.Atoi(clientUserinfoChanged[0])
-					panicIf(err)
-					id--
-					nome := clientUserinfoChanged[1]
-
-					if playerListContainsId(quakeGames[gameCount].Status.Players.List, id) {
-						pl := getPlayerIndexByIdFromPlayerList(quakeGames[gameCount].Status.Players.List, id)
-
-						if !playerListContainsNome(quakeGames[gameCount].Status.Players.List, nome) {
-							if !oldNamesContainsNome(quakeGames[gameCount].Status.Players.List[pl].OldNames, quakeGames[gameCount].Status.Players.List[pl].Nome) {
-								quakeGames[gameCount].Status.Players.List[pl].OldNames =
-									append(quakeGames[gameCount].Status.Players.List[pl].OldNames, quakeGames[gameCount].Status.Players.List[pl].Nome)
-							}
-							quakeGames[gameCount].Status.Players.List[pl].Nome = nome
-						}
-						if !playerListContainsNome(quakeGames[gameCount].Status.Players.List, nome) {
-							quakeGames[gameCount].Status.Players.List[pl].Nome = nome
-						}
-						quakeGames[gameCount].Status.Players.List[pl].OldNames =
-							cleanRepeatedOldNames(quakeGames[gameCount].Status.Players.List[pl].OldNames, quakeGames[gameCount].Status.Players.List[pl].Nome)
-					} else {
-						quakeGames[gameCount].Status.Players.List = append(quakeGames[gameCount].Status.Players.List, Player{Nome: nome, Id: id})
-					}
-
-					// fmt.Printf("id{%d} type:%T, nome{%s} type:%T\n", id, id, nome, nome)
-
-				}
-			case "Kill:":
-				{
-					quakeGames[gameCount].Status.TotalKills++
-					kill := strings.Split(killRE.ReplaceAllString(v, `$6   :   $7   :   $8`), "   :   ")
-					killer, victim := kill[0], kill[1]
-					//cause := kill[2]
-					// fmt.Printf("v{%s} k{%s} c{%s}\n", victim, killer, cause)
-					for k, p := range quakeGames[gameCount].Status.Players.List {
-						if killer != "<world>" && killer == p.Nome {
-							quakeGames[gameCount].Status.Players.List[k].Kills++
-						}
-					}
-					if killer == "<world>" {
-						for k, p := range quakeGames[gameCount].Status.Players.List {
-							if p.Nome == victim {
-								quakeGames[gameCount].Status.Players.List[k].Kills--
-							}
-
-						}
-					}
-				}
-			}
-		}
-	}
-
-	for _, v := range quakeGames {
-		fmt.Println(v)
-	}
-
+	return quakeGames
 }
 
 func getFileValues(file *os.File) []string {
@@ -176,13 +96,13 @@ func playerListContainsNome(arr []Player, item string) bool {
 }
 
 func getPlayerIndexByIdFromPlayerList(arr []Player, id int) int {
-	var index int
+	var playerIndex int
 	for k, v := range arr {
 		if v.Id == id {
-			index = k
+			playerIndex = k
 		}
 	}
-	return index
+	return playerIndex
 }
 
 func oldNamesContainsNome(arr []string, nome string) bool {
@@ -205,4 +125,81 @@ func cleanRepeatedOldNames(arr []string, currentName string) []string {
 		}
 	}
 	return cleanedOldNames
+}
+
+func parseDataFromFileLines(logLines []string, qgl []QuakeGameLog) []QuakeGameLog {
+	gameCount := 0
+	for _, v := range logLines {
+		lineContent := strings.Split(v, " ")
+		for _, y := range lineContent {
+			switch y {
+			case "InitGame:":
+				{
+					qgl = append(qgl, QuakeGameLog{Game: gameCount + 1})
+				}
+			case "ShutdownGame:":
+				{
+					gameCount++
+				}
+			case "ClientUserinfoChanged:":
+				{
+					var clientUserinfoChangedRE *regexp.Regexp
+					clientUserinfoChangedRE, err := regexp.Compile(`^(.*) (\d+)( n\\)(.+)(\\t\\)(.*)$`)
+					panicIf(err)
+					clientUserinfoChanged := strings.Split(clientUserinfoChangedRE.ReplaceAllString(v, `$2   :   $4`), "   :   ")
+					id, err := strconv.Atoi(clientUserinfoChanged[0])
+					panicIf(err)
+					id--
+					nome := clientUserinfoChanged[1]
+
+					if playerListContainsId(qgl[gameCount].Status.Players.List, id) {
+						pl := getPlayerIndexByIdFromPlayerList(qgl[gameCount].Status.Players.List, id)
+
+						if !playerListContainsNome(qgl[gameCount].Status.Players.List, nome) {
+							if !oldNamesContainsNome(qgl[gameCount].Status.Players.List[pl].OldNames, qgl[gameCount].Status.Players.List[pl].Nome) {
+								qgl[gameCount].Status.Players.List[pl].OldNames =
+									append(qgl[gameCount].Status.Players.List[pl].OldNames, qgl[gameCount].Status.Players.List[pl].Nome)
+							}
+							qgl[gameCount].Status.Players.List[pl].Nome = nome
+						}
+						if !playerListContainsNome(qgl[gameCount].Status.Players.List, nome) {
+							qgl[gameCount].Status.Players.List[pl].Nome = nome
+						}
+						qgl[gameCount].Status.Players.List[pl].OldNames =
+							cleanRepeatedOldNames(qgl[gameCount].Status.Players.List[pl].OldNames, qgl[gameCount].Status.Players.List[pl].Nome)
+					} else {
+						qgl[gameCount].Status.Players.List = append(qgl[gameCount].Status.Players.List, Player{Nome: nome, Id: id})
+					}
+
+					// fmt.Printf("id{%d} type:%T, nome{%s} type:%T\n", id, id, nome, nome)
+
+				}
+			case "Kill:":
+				{
+					var killRE *regexp.Regexp
+					killRE, err := regexp.Compile(`^(.*)( Kill: )(\d+) (\d+) (\d+:) (.+) killed (.+) by (.*)$`)
+					panicIf(err)
+					qgl[gameCount].Status.TotalKills++
+					kill := strings.Split(killRE.ReplaceAllString(v, `$6   :   $7   :   $8`), "   :   ")
+					killer, victim := kill[0], kill[1]
+					//cause := kill[2]
+					// fmt.Printf("v{%s} k{%s} c{%s}\n", victim, killer, cause)
+					for k, p := range qgl[gameCount].Status.Players.List {
+						if killer != "<world>" && killer == p.Nome {
+							qgl[gameCount].Status.Players.List[k].Kills++
+						}
+					}
+					if killer == "<world>" {
+						for k, p := range qgl[gameCount].Status.Players.List {
+							if p.Nome == victim {
+								qgl[gameCount].Status.Players.List[k].Kills--
+							}
+
+						}
+					}
+				}
+			}
+		}
+	}
+	return qgl
 }
